f5e8e3ab0251ac407259c11c80a45f3df522a0e5
kristiyanto <danielkr@uw.edu>
2015-01-12 18:19:01 -0800

    I think it's working now...

--- TCSS558/src/client.c
+++
@@ -1,4 +0,0 @@
-int client_main(void) {
-	puts("!!!Hello World from the Client!!!"); /* prints !!!Hello World!!! */
-	return 0;
-}
---
+++ src/log.h
@@ -0,0 +1,34 @@
+/*
+ * log.h
+ *
+ *  Created on: Jan 11, 2015
+ *      Author: kevanderson
+ */
+#ifndef SERVER_H
+  #include "server.h"
+#endif
+
+#ifndef LOG_H
+  #define LOG_H
+  #define S_LOGFILE "server.log"
+#endif
+
+
+
+/*******************************************************************************
+ * WRITES A WELL FORMED MESSAGE AND RESPONSE TO THE SERVER.LOG FILE THAT WILL  *
+ * INCLUDE A TIMESTAMP OF THE WRITE.  IF THE SERVER.LOG FILE DOES NOT EXIST IT *
+ * WILL BE CREATED.  IF THE FILE IS UNABLE TO BE OPENED, LOG_WRITE WILL RETURN *
+ * -1 OTHERWISE WILL RETURN 0.                                                 *
+ ******************************************************************************/
+int log_write(char * host, int port, char * message, char * response);
+
+
+/*******************************************************************************
+ * A HELPER FUNCTION THAT RETURNS A 30 CHARACTER ARRAY REPRESENTING THE        *
+ * CURRENT TIME WITH MILLISECOND PRECISION.  THE TIMESTAMP IS PROVIDED IN      *
+ * LOCAL TIME.  THE FORMAT OF THE TIMESTAMP IS AS FOLLOWS:                     *
+ * (TMZ) YYYY-MM-DD HH:MM:SS.SSS (e.g. (PDT) 2015-01-31 23:59:52.123           *
+ * FOR MILLISECOND PRECISION, POSIX IS ASSUMED.                                *
+ ******************************************************************************/
+void buildTimeStamp(char * output);
