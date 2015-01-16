4079ff1ce9b9f176f8623ff3446c230b4e710749
Kevin Anderson <k3a@uw.edu>
2015-01-15 18:32:05 -0800

    sync

--- src/main.c
+++ src/main.c
@@ -22,13 +22,13 @@ int main(int argc, char *argv[]) {
 	udp_server_main(argc, argv);
 
 	// to test a TCP SERVER
-	tcp_server_main(argc, argv);
+//	tcp_server_main(argc, argv);
 
 	// to test a UDP CLIENT
-	udp_client_main(argc, argv);
+//	udp_client_main(argc, argv);
 
 	// to test a TCP CLIENT
-	tcp_client_main(argc, argv);
+//	tcp_client_main(argc, argv);
 
 /*  OLD CODE THAT TESTS THE RUN ARGUMENTS
 	if (strcmp(argv[0], "client") == 0)
