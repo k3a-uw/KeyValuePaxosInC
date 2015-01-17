90d70cd9f6020ba27004498d0b8037fa17d662de
kristiyanto <danielkr@uw.edu>
2015-01-16 16:42:54 -0800

    Progress for UDP Server. It still have few problems: - The port binding is locked for a certain time. So once, stopped, it may be a good idea to change the port number hstols() into a random other number - I still haven't figured out how to pass the argv to the port number. - The message passing to the keyvalue

--- src/udp_server.c
+++ src/udp_server.c
@@ -1,62 +1,81 @@
 
 
 #ifndef UDP_SERVER_H
-#include "udp_server.h"
+	#include "udp_server.h"
 #endif
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#ifndef STDIO_H
+	#include <stdio.h>
+#endif
 
-#define BUFFER_SIZE 1024
-#define SERVER_PORT 7777
+#ifndef TYPES_H
+	#include <sys/types.h>
+#endif
+
+#ifndef SOCKET_H
+	#include <sys/socket.h>
+#endif
+
+#ifndef NETINET_H
+	#include <netinet/in.h>
+#endif
+
+#ifndef INET_H
+	#include <arpa/inet.h>
+#endif
 
 int udp_server_main(int argc, char *argv[])
 {
-	//printf("You're testing the UDP Server!\n");
-	int sockfd, listening, rc,accepting;
-	struct sockaddr_in serveraddress;
+	printf("You're testing the UDP Server!\n Usage: udpserver port");
+	printf(" \n Example: udpserver 7000 \n");
 
-	// AF INET, to receviee incoming connection
-	listening=socket(AF_INET,SOCK_DGRAM,0);
-	if (listen<0)
+	int sockfd, rc, len, n;
+	struct sockaddr_in serveraddress;
+	struct sockaddr_in clientaddress;
+	char message[20];
+
+	//uint16_t serverport = 898989;
+	if(argc>1)
 	{
-		printf("Error");
+		//serverport=atoi(argv[1]);
+	}
+	//printf("UDP Client - Usage %s <Server hostname or IP>\n", serverport);
+
+	// AF INET, to receive incoming connection
+	sockfd=socket(AF_INET,SOCK_DGRAM,0);
+	if (sockfd<0)
+	{
+		printf("Error setting up socket socket \n");
 		exit(-1);
+	}
+	else
+	{
+		printf("Setting the Socket \n");
 	}
 	 // BIND IT
-	memset(&serveraddress,0, sizeof(serveraddress));
+	bzero(&serveraddress, sizeof(serveraddress));
 	serveraddress.sin_family = AF_INET;
 	serveraddress.sin_addr.s_addr = htonl(INADDR_ANY);
-	serveraddress.sin_port = htons(SERVER_PORT);
-	rc = bind(listening, (struct sockaddr *)&serveraddress, sizeof(serveraddress));
-	if (bind<0)
-	{
-		printf("Error");
-		close(listening);
-		exit(-1);
-	}
-	rc = listen(listening, 5);
+	serveraddress.sin_port = htons(32119);
+	rc = bind(sockfd, (struct sockaddr *)&serveraddress, sizeof(serveraddress));
 	if (rc<0)
 	{
-		printf("Error");
-		close(listening);
+		printf("Binding Error. \n");
+		close(sockfd);
 		exit(-1);
+	}else
+	{
+		printf("Binding the socket. \n");
 	}
-
 	while(1)
 	{
-		accepting = accept(listening, NULL, NULL);
-		if (listening<0)
-		{
-			printf("Error");
-			close(listening);
-			exit(-1);
-		}
-	}
+		len = sizeof(clientaddress);
+		n = recvfrom(sockfd,message,1000,0,(struct sockaddr *)&clientaddress,&len);
+		sendto(sockfd,message,n,0,(struct severaddress *)&clientaddress,sizeof(clientaddress));
+		message[n] = 0;
+		printf("%s",message);
+	 }
+	printf("finished");
 
-	return 0;
 
 }
